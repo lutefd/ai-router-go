d71cce4f666b2891aab44a3656f6650c1502709f
Luis Dourado <luis.dourado@hurb.com>
2025-02-07 13:11:59 -0300

    feat(routes): update routes setup to add oauth routes

--- internal/server/routes.go
+++ internal/server/routes.go
@@ -5,11 +5,16 @@ import (
 	"github.com/lutefd/ai-router-go/internal/handler"
 )
 
-func routes(handler *handler.AIHandler) chi.Router {
+func routes(handler *handler.AIHandler, authHandler *handler.AuthHandler) chi.Router {
 
 	r := chi.NewRouter()
 
 	r.Route("/api/v1", func(r chi.Router) {
+		r.Route("/auth", func(r chi.Router) {
+			r.Get("/google/login", authHandler.GoogleLogin)
+			r.Get("/google/callback", authHandler.GoogleCallback)
+		})
+
 		r.Route("/ai", func(r chi.Router) {
 			r.Route("/generate", func(r chi.Router) {
 				r.Post("/", handler.ProxyRequest)
