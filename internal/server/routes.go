7de6d4f6f125ced3fc3f6ff711b8a5aeea4c0040
Luis Dourado <luis.dourado@hurb.com>
2025-02-07 10:52:23 -0300

    feat(server): add routing config

--- internal/server/routes.go
+++ internal/server/routes.go
@@ -2,14 +2,19 @@ package server
 
 import (
 	"github.com/go-chi/chi/v5"
+	"github.com/lutefd/ai-router-go/internal/handler"
 )
 
-func routes() chi.Router {
+func routes(handler *handler.AIHandler) chi.Router {
 
 	r := chi.NewRouter()
 
 	r.Route("/api/v1", func(r chi.Router) {
-
+		r.Route("/ai", func(r chi.Router) {
+			r.Route("/generate", func(r chi.Router) {
+				r.Post("/", handler.ProxyRequest)
+			})
+		})
 	})
 
 	return r
