ec1ea1adbf710d962f3c4287c3bda06dea76861e
Luis Dourado <luis.dourado@hurb.com>
2025-02-08 07:47:31 -0300

    feat(routes): add logger and recoverer middleware and configure allow all cors

--- internal/server/routes.go
+++ internal/server/routes.go
@@ -2,6 +2,8 @@ package server
 
 import (
 	"github.com/go-chi/chi/v5"
+	chi_middleware "github.com/go-chi/chi/v5/middleware"
+	"github.com/go-chi/cors"
 	"github.com/lutefd/ai-router-go/internal/handler"
 	"github.com/lutefd/ai-router-go/internal/middleware"
 )
@@ -9,6 +11,18 @@ import (
 func routes(handler *handler.AIHandler, authHandler *handler.AuthHandler, chatHandler *handler.ChatHandler, userHandler *handler.UserHandler, healthHandler *handler.HealthHandler, authMiddleware *middleware.AuthMiddleware) chi.Router {
 
 	r := chi.NewRouter()
+
+	r.Use(chi_middleware.Logger)
+	r.Use(chi_middleware.Recoverer)
+
+	r.Use(cors.Handler(cors.Options{
+		AllowedOrigins:   []string{"*"},
+		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowedHeaders:   []string{"Accept", "Authorization", "Content-Type", "X-CSRF-Token", "X-Refresh-Token"},
+		ExposedHeaders:   []string{"Link"},
+		AllowCredentials: true,
+		MaxAge:           300,
+	}))
 
 	r.Get("/healthz", healthHandler.LivenessCheck)
 	r.Get("/readiness", healthHandler.ReadinessCheck)
